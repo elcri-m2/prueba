b865ea88932fafe4eededa85ce105f4494afce87
Cristian Mieles <cmielesm@unal.edu.co>
2024-04-23 07:32:25 -0500

    primera subida

---
+++ src/main/java/com/mycompany/pruebagithub/prueba.java
@@ -0,0 +1,16 @@
+package com.mycompany.pruebagithub;
+
+/**
+ *
+ * @author cmiel
+ */
+public class prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Esto se va a subir a GitHub :)");
+    }
+    
+}
